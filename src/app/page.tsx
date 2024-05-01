30e2a3b39830b44ca665c64134ff585653c8cb54
Aditya <codecommander03@gmail.com>
2024-05-01 21:54:14 +0530

    file upload compnent added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { auth, UserButton } from "@clerk/nextjs";
 import { LogIn } from "lucide-react";
@@ -28,7 +29,7 @@ export default async function Home() {
           <div className="w-full mt-4">
             {isAuth ? (
               <FileUpload />
-            ) : (
+            ): (
               <Link href="/sign-in">
                 <Button>
                   Login to get Started!
